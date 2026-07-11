a794bc02650a2b67cc14810a355a845efb8929bc
mehrab un nisa <meerabkhanmalik1@gmail.com>
2026-07-11 05:21:15 -0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBamlpjQ66SvBeAbwkvrzWld3Y9Z6lsOGc",
-  authDomain: "anastasis-technologies-pvt-ltd.firebaseapp.com",
-  projectId: "anastasis-technologies-pvt-ltd",
-  storageBucket: "anastasis-technologies-pvt-ltd.firebasestorage.app",
-  messagingSenderId: "1076972692429",
-  appId: "1:1076972692429:web:7f4d7200b1f3a705aeda84",
+    apiKey: "AIzaSyYOUR_ACTUAL_API_KEY_HERE",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef123456789"
 };
 
-// Initialize App Node instances
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
